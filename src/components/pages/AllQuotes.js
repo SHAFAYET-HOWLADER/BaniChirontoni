717aaddafdd4c3f0e202cdf6973f205077e98c02
Safayet-Howlader <safayethowlader96@gmail.com>
2022-08-03 02:31:14 +0300

    data added

--- src/components/pages/AllQuotes.js
+++ src/components/pages/AllQuotes.js
@@ -1,6 +1,9 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+const axios = require('axios');
 const AllQuotes = () => {
+  const [quotes, setQuotes] = useState([]);
+  axios.get('http://localhost:5000/getQuotes')
+  .then(res=> setQuotes(res))
   return (
     <div>AllQuotes</div>
   )
